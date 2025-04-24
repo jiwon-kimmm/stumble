e5b3e387dc3778d665aacc0fd7a9962a38b6dfc5
Jiwon <jiwonkim1207@gmail.com>
2025-04-24 23:29:59 +0900

    Added database

--- app/src/main/java/com/example/stumble/MainActivity.java
+++ app/src/main/java/com/example/stumble/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.stumble;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
@@ -32,7 +33,13 @@ public class MainActivity extends AppCompatActivity {
         registerPermissionCallback();
         requestPermissions();
 
-//        KakaoMapSdk.iniy
+        ((Button)findViewById(R.id.SignUpLoginButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 
     private void registerPermissionCallback() {
